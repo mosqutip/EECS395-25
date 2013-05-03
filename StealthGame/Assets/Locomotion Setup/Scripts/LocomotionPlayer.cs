af6def97892e677cc95b671934e3341f4e716dea
jcc333 <clemer19@gmail.com>
2013-05-03 09:56:54 -0500

    Animate the hero; add a camera.

---
+++ StealthGame/Assets/Locomotion Setup/Scripts/LocomotionPlayer.cs
@@ -0,0 +1,39 @@
+ /// <summary>
+/// 
+/// </summary>
+
+using UnityEngine;
+using System;
+using System.Collections;
+  
+[RequireComponent(typeof(Animator))]  
+
+public class LocomotionPlayer : MonoBehaviour {
+
+    protected Animator animator;
+
+    private float speed = 0;
+    private float direction = 0;
+    private Locomotion locomotion = null;
+
+	void Start () 
+	{
+        animator = GetComponent<Animator>();
+        locomotion = new Locomotion(animator);
+	}
+    
+	void Update () 
+	{
+        if (animator && Camera.main)
+		{
+            JoystickToEvents.Do(transform,Camera.main.transform, ref speed, ref direction);
+            
+			if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+				locomotion.Do(speed * 8, direction * 180);
+			}
+			else {
+				locomotion.Do(speed, direction * 180);
+			}
+		}		
+	}
+}
