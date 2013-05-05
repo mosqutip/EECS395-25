fd290f4e1d775d1a04a66143813eeba903c61baf
Josiah Matlack <mosqutip@gmail.com>
2013-05-05 17:29:05 -0500

    Added walls and slightly moved door

--- StealthGame/Assets/EndConditions.cs
+++ StealthGame/Assets/EndConditions.cs
@@ -8,10 +8,6 @@ public class EndConditions : MonoBehaviour {
 		if (other.gameObject.name == "door_4_1")
 		{
 			int nextLevel = Application.loadedLevel + 1;
-//			int counter = Convert.ToInt32(nextLevel.Substring((nextLevel.Length - 1), 1));
-//			nextLevel = nextLevel.Substring (0, (nextLevel.Length - 1));
-//			nextLevel += counter;
-//			Debug.Log(nextLevel);
 			Application.LoadLevel(nextLevel);
 		}
 	}
