0196e50f539796688240d58e664d983e583958f4
John Greene <JohnGreene2012@u.northwestern.edu>
2013-05-06 13:36:25 -0500

    fixed lighting issue added new camera
    The old camera still exists the new one uses a different script in case
    we like the old one better

--- StealthGame/Assets/SteathScripts/FollowCamera.cs
+++ StealthGame/Assets/SteathScripts/FollowCamera.cs
@@ -3,14 +3,56 @@ using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
     public GameObject target;
-    Vector3 offset;
+	public float y;
+	public float xoffset;
+	public float zoffset;
+	public int startingRotation;
+    //Vector3 offset;
+	int currentRotation;
+	int zinvert = 1;
+	int xinvert = 1;
+	
     void Start() {
-        offset = target.transform.position - transform.position;
-    }
+        //offset = target.transform.position - transform.position;
+		currentRotation = startingRotation;
+	}
+	
 	void LateUpdate() {
-	    float desiredAngle = target.transform.eulerAngles.y;
-    	Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-		transform.position = target.transform.position - (rotation * offset);
-		transform.LookAt(target.transform);
+		if (Input.GetKeyDown ("q")){
+			currentRotation += 90;
+			if (currentRotation >= 360){
+				currentRotation -= 360;
+			}
+		}
+		if (Input.GetKeyDown ("e")){
+			currentRotation -= 90;
+			if (currentRotation <= -360){
+				currentRotation += 360;
+			}
+		}
+		if ((currentRotation == startingRotation) || (currentRotation == startingRotation - 360)){
+				zinvert = 1;
+				xinvert = 1;
+		}
+		else if ((currentRotation == startingRotation + 90) || (currentRotation == startingRotation - 270)){
+				zinvert = -1;
+				xinvert = 1;
+		}
+		else if ((currentRotation == startingRotation + 180) || (currentRotation == startingRotation - 180)){
+				zinvert = -1;
+				xinvert = -1;
+		}
+		else {
+				zinvert = 1;
+				xinvert = -1;
+		}		
+					
+		
+	    //float desiredAngle = target.transform.eulerAngles.y;
+    	//Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, currentRotation, transform.eulerAngles.z);
+		Vector3 newPosition = new Vector3(target.transform.position.x - xoffset * xinvert, y, target.transform.position.z - zoffset * zinvert);
+		transform.position = newPosition;
+		//transform.LookAt(target.transform);
 	}
 }
