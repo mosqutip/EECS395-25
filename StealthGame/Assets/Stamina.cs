2ac14a0160eb4830780f838ff67fb88f76a14d0e
jcc333 <clemer19@gmail.com>
2013-05-14 18:58:26 -0500

    Add player noise and variation

--- StealthGame/Assets/Stamina.cs
+++ StealthGame/Assets/Stamina.cs
@@ -7,21 +7,35 @@ public class Stamina : MonoBehaviour
 	private float xOffset = (Screen.width * 0.25f), yOffset = (Screen.height * 0.05f),
 						    screenScaleX = (Screen.width / 1024.0f), screenScaleY = (Screen.height / 768.0f);
 	private Rect emptyBarRect;
+	private PlayerMovement move;
+	
 	public float stamina;
 	public Texture emptyBar, fullBar;
 	public float speed;
 	public float rechargeSpeed;
-
+	
+	
 	void Start()
 	{
 		emptyBarRect = new Rect((xSize - xOffset - 5.0f), (ySize - yOffset - 5.0f), xOffset, yOffset);
+		move = FindPlayer().GetComponent<PlayerMovement>();
+	}
+	
+	private GameObject FindPlayer()
+	{
+		foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Player"))
+		{
+			if (obj.name == "carl")
+			{
+				return obj;
+			}
+		}
+		return null;
 	}
 	
 	void Update()
 	{
-		PlayerMovement playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
-		
-		if (playerMovement.sprint && !playerMovement.sneak && playerMovement.speed > 0)
+		if (move.sprint && !move.sneak && move.speed > 0)
 		{
 			if (stamina > 0)
 			{
