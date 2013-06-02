af04e9086b382e58f4cf84d51a04c6a641dedcb8
jcc333 <clemer19@gmail.com>
2013-06-02 17:36:24 -0500

    Explosions, victory

--- StealthGame/Assets/VictoryScript.cs
+++ StealthGame/Assets/VictoryScript.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class VictoryScript : MonoBehaviour
 {
+	private GUIStyle victoryStyle;
 	
-	public Texture vicPic;
+	void Start () {
+		victoryStyle = new GUIStyle();
+		victoryStyle.fontSize = 30;
+	}
 	
 	void OnGUI()
 	{
 		
-		GUI.DrawTexture(new Rect(Screen.width/2 - 160, 160, 320, 320), vicPic);
-		transform.Translate(0, 0, 0);
+		GUI.Label(new Rect (Screen.width/2 - 65, Screen.height/4 - 15, 100, 30), "VICTORY!", victoryStyle);
 		if (GUI.Button (new Rect (Screen.width/2 - 50, Screen.height/2 - 15, 100, 30), "Play Again"))
 		{
 			Application.LoadLevel("Escape");
